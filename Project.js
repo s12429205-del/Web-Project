5e4774acd8ebead116fc9b5ed3f966822f136a36
Tabarak Barahmeh <s12429637@stu.najah.edu>
2025-12-16 17:24:15 -0700

    commit

--- Project.js
+++ Project.js
@@ -1,3 +1,6 @@
+setTimeout(function (){
+    document.getElementById("Jerusalem").style.display="none";
+},5000);
 document.addEventListener('DOMContentLoaded', function () {
     const navToggle = document.querySelector('.nav-toggle');
     const navUl = document.querySelector('nav ul');
